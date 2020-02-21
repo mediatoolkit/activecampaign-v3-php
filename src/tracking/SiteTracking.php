150bf4e38a832b333a56fb6627972df60eb8d952
Matt Glover <matt@sparkeleven.com.au>
2020-02-21 23:27:44 +1100

    domain whitelist, get tracking code, set tracking status

--- src/tracking/SiteTracking.php
+++ src/tracking/SiteTracking.php
@@ -13,8 +13,51 @@ class SiteTracking extends Resource
 {
 
     /**
+     * Add a domain to the site tracking whitelist
+     * @see https://developers.activecampaign.com/reference#add-domain-to-whitelist
+     *
+     * @param string $domain_name
+     * @return string
+     */
+    public function addDomainToWhitelist($domain_name)
+    {
+        $req = $this->client
+            ->getClient()
+            ->post('api/3/siteTrackingDomains', [
+                'json' => [
+                    'siteTrackingDomain' => [
+                        'name' => $domain_name
+                    ]
+                ]
+            ]);
+
+        return $req->getBody()->getContents();
+    }
+
+
+    /**
+     * Get site tracking code (javascript snippet)
+     * @see https://developers.activecampaign.com/reference#retrieve-site-tracking-code
+     *
+     * @param array $query_params
+     * @return string
+     */
+    public function retrieveSiteTrackingCode(array $query_params = [])
+    {
+        $req = $this->client
+            ->getClient()
+            ->get('api/3/siteTracking/code', [
+                'query' => $query_params
+            ]);
+
+        return $req->getBody()->getContents();
+    }
+
+
+    /**
      * Get site tracking status (enabled or disabled)
      * @see https://developers.activecampaign.com/reference#retrieve-site-tracking-status
+     *
      * @param array $query_params
      * @return string
      */
@@ -28,5 +71,30 @@ class SiteTracking extends Resource
 
         return $req->getBody()->getContents();
     }
+
+
+    /**
+     * Enable or disable site tracking
+     * @see https://developers.activecampaign.com/reference#enable-disable-site-tracking
+     *
+     * @param boolean $enabled
+     * @return string
+     */
+    public function setTrackingStatus($enabled = true)
+    {
+        $req = $this->client
+            ->getClient()
+            ->put('api/3/siteTracking', [
+                'json' => [
+                    'siteTracking' => [
+                        'enabled' => $enabled === true ? true : false
+                    ]
+                ]
+            ]);
+
+        return $req->getBody()->getContents();
+    }
+
+
 
 }
